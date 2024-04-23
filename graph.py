2de98a2cf1ae6384f4792c4295e7ef4dab49e995
ninad2612 <167805209+ninad2612@users.noreply.github.com>
2024-04-24 04:20:48 +0530

    Delete graph.py

--- graph.py
+++
@@ -1,8 +0,0 @@
-graph = {}
-num_nodes = int(input("Enter the number of nodes in the graph: "))
-for i in range(num_nodes):
-    node_str = input(f"Enter the neighbors for node {i}: ")
-    neighbors = [int(x) for x in node_str.split()]
-    graph[i] = neighbors
-
-print(graph)
